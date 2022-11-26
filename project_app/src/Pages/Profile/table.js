8cab35eac70468b4d080ba996122703e3eb00bbd
subhamadhikari <subhamadhikari7@gmail.com>
2022-11-26 12:28:13 +0545

    commit

---
+++ project_app/src/Pages/Profile/table.js
@@ -0,0 +1,9 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+export {Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Paper}
--- project_app/src/Pages/index.js
+++ project_app/src/Pages/index.js
@@ -1,4 +1,7 @@
 import Home from "./Home/Home"
 import Login from "./Login/Login"
+import Farmer from "./Farmer/Farmer"
+import Profile from "./Profile/Profile"
 
-export {Home,Login}
+
+export {Home,Login,Profile,Farmer}
